5e7e20bad5635cadf5bcfb81bc3f42ff9d457ad2
Žan <zan1303@hotmail.com>
2024-01-11 00:51:51 +0100

    adding project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import GalleryPage from './Components/GalleryPage/GalleryPage.component';
+import Navi from './Components/Navbar/Navbar.component';
+import Home from './Components/Home/Home.component';
+import Kontakti from './Components/Kontakti/Kontakti.component';
 
-function App() {
+
+
+  
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    
+      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navi/>} >
+            <Route index element= {<Home/>}/>
+            <Route path='/home' element={<Home/>} />
+            <Route path='/gallery' element={<GalleryPage/>} /> 
+            <Route path='/kontakti' element={<Kontakti/>} />
+          </Route>
+        </Routes>
+      </Router>
+
+    
   );
 }
 
 export default App;
+
+
